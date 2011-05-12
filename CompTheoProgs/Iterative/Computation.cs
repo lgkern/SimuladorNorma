6db96742346458ad391cee16be5d73633cb92065
Guilherme Azzi <ggazzi@inf.ufrgs.br>
2011-05-12 11:34:52 -0300

    Added comments and refactored.
    Added comments to all the main interfaces.
    
    Refactored the computation abstract class
    so its subclasses don't need to know
    about the observer pattern.

--- CompTheoProgs/Iterative/Computation.cs
+++ CompTheoProgs/Iterative/Computation.cs
@@ -5,35 +5,48 @@ using System.Text;
 
 namespace CompTheoProgs.Iterative
 {
+    /*  A computation for iterative programs.
+     * 
+     *  Executes "recursively" on the structure of
+     * the program, as defined formally.
+     * 
+     *  Uses whole programs (on a single line)
+     * to identify each computation step.
+     */ 
     class Computation : CompTheoProgs.Computation
     {
         private Composition currentProgram;
 
-        public string CurrentState
-        {
-            get { return machine.CurrentState; }
-        }
-
-        public Computation(Program prog, IMachine mach)
+        /*  Composes the program to be executed with the empty program,
+         * as required in the formal definition, and records the inital
+         * computation step.
+         */
+        public Computation(Program prog, IMachine mach) : base(mach)
         {
             currentProgram = new Composition( prog, new Empty() );
-            machine = mach;
+
             AddStep(currentProgram.ToString(), mach.CurrentState);
         }
 
+        /*  Executes the computation "recursively" on its structure, as defined formally.
+         * Such execution is delegated from here to the Composition class, and there
+         * to each program class.
+         */
         public override void RunStep()
         {
-            if (finished)
+            if (Finished)
                 throw new EndOfComputationException("Cannot run steps after the computation ended");
 
+            // Delegates the execution to the composition class
             currentProgram.RunComputationStep(machine);
             AddStep(currentProgram.ToString(), machine.CurrentState);
 
-            if (currentProgram.Subprograms[0].GetType() == typeof(Empty)) 
+            /* When there's only one program on the composition, it is
+             * certainly the empty one and thus the computation must end.
+             */
+            if (currentProgram.Subprograms.Count == 1)
             {
-                result = machine.GetValue();
-                NotifyObserversOfResult(result);
-                finished = true;
+                SetResult(machine.GetValue());
             }
         }
     }
